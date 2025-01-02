1ead58484d487c9026a02fe571f39a755d4d25d3
DreamBot706 <187842058+DreamBot706@users.noreply.github.com>
2025-01-02 16:15:42 +0000

    Add beautiful UI

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-let humanscore = 0;
-let computerscore = 0;
+// the scores:
+let human_score = document.querySelector(".violet")
+let computer_score = document.querySelector(".orange")
 
+// the display pictures
+let human_choice = document.querySelector(".human.container > img")
+let computer_choice = document.querySelector(".computer.container > img")
 
-function getComputerChoice(){
-    return Math.floor( Math.random() * 3);
-}
+// the buttons
+let rock_button = document.querySelector(".rock.button")
+let paper_button = document.querySelector(".paper.button")
+let scissors_button = document.querySelector(".scissors.button")
 
-function getHumanChoice(){
-    let choice =  prompt("What do you wish to play (Rock / Paper / Scissors)");
-    switch (choice.toLowerCase()){
-        case ("rock"):
-            return 0;
-        case ("paper"):
-            return 1;
-        case ("scissors"):
-            return 2;
-    }
-}
+// the image locations
+let rock_image = "./images/rock.jpg"
+let paper_image = "./images/paper.jpg"
+let scissors_image = "./images/scissors.jpg"
+let arrayofimages = [rock_image, paper_image, scissors_image]
 
-function convertor(code){
-    switch (code){
+// 0: rock; 1: paper; 2: scissors
+
+// function to convert the code into text
+function convertor(code) {
+    switch (code) {
         case 0:
             return "Rock"
         case 1:
@@ -29,31 +31,62 @@ function convertor(code){
     }
 }
 
-function playRound( human, computer ){
+// function to get the computer choice
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
+}
+
+// function logic of one round
+function playRound(human, computer) {
+
+    human_choice.src = arrayofimages[human]
+    computer_choice.src = arrayofimages[computer]
+
     let winner_message = `You win! ${convertor(human)} beats ${convertor(computer)}`;
     let loser_message = `You lose! ${convertor(human)} loses to ${convertor(computer)}`;
     let draw_message = `It's a tie! Human and Computer both play ${convertor(human)}`
     let diff = human - computer;
 
     if (human == computer)
-        return draw_message;
-    else if ( diff == 1 || diff == -2){
-        ++humanscore;
-        return winner_message;}
+        alert(draw_message)
+    else if (diff == 1 || diff == -2) {
+        human_score.textContent = `${Number(human_score.textContent) + 1}`
+        alert(winner_message)
+    }
     else {
-        ++computerscore;
-        return loser_message}
+        computer_score.textContent = `${Number(computer_score.textContent) + 1}`
+        alert(loser_message)
+    }
+
+
 }
 
-
-function playGame() {
-    for (let i = 1; i != 6; ++i){
-        console.log(`Round ${i}! \n\n${playRound(getHumanChoice(), getComputerChoice())}\n\nThe Scores currently are: \nHuman: ${humanscore}\nComputer: ${computerscore}\n`)
+function isGameOver() {
+    if (human_score.textContent == '5') {
+        alert("Congratulations you have won!")
+        location.reload(true)
+    }
+    else if (human_score.textContent == '5') {
+        alert("Uh-oh you have lost unfortunately!")
+        location.reload(true)
     }
 }
+rock_button.addEventListener("click", () => { playRound(0, getComputerChoice()); isGameOver()})
+paper_button.addEventListener("click", () => { playRound(1, getComputerChoice()); isGameOver()})
+scissors_button.addEventListener("click", () => { playRound(2, getComputerChoice()); isGameOver() })
 
-playGame();
 
-
-
-
+// playing the actual game
+// while (true) {
+//     rock_button.addEventListener("click", playRound(0, getComputerChoice))
+//     paper_button.addEventListener("click", playRound(1, getComputerChoice))
+//     scissors_button.addEventListener("click", playRound(2, getComputerChoice))
+//     if (human_score.textContent == '5') {
+//         alert("Congratulations. You have won !")
+//         break
+//     }
+//     else if (computer_score.textContent == '5') {
+//         alert("Rip the computer has won, well at least you tried!")
+//         break
+//     }
+// }
